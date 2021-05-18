bd4bd94d49fe3ffa7c0c2bb740b60b58857d5f76
jovilleneuve <joblo14_01@hotmail.com>
2021-05-18 00:25:24 -0400

    Add files via upload

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Names
+{
+    /// <summary>
+    /// Logique d'interaction pour MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (!string.IsNullOrWhiteSpace(txtName.Text) && !listName.Items.Contains(txtName.Text)) {
+                listName.Items.Add(txtName.Text);
+            }
+
+
+
+
+        }
+
+
+        private void Remove(object sender, RoutedEventArgs e)
+        {
+            listName.Items.Remove(listName.SelectedItem);
+        }
+       
+    }
+}
